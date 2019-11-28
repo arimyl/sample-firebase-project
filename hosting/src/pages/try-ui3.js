3c6f10969b35aaba41c60be1bd7a8bc2949c6728
ali <ali.myl@example.com>
2019-11-28 20:00:24 +0900

    20191128-start

--- hosting/src/pages/try-ui3.js
+++ hosting/src/pages/try-ui3.js
@@ -2,13 +2,14 @@ import React from "react";
 import { css } from "@emotion/core";
 import { makeStyles } from '@material-ui/core/styles';
 import { Button, IconButton, Select, FormControl} from '@material-ui/core';
-import {ListItem} from '@material-ui/core';
+import {ListItem, TextField} from '@material-ui/core';
 
 const useStyles = makeStyles(theme =>({
   root:{
     flexGrow: 1,
   },
   formControl:{
+    
     marginRight: theme.spacing(1),
     minWidth: 120,
   },
@@ -29,6 +30,7 @@ export function SelectLang () {
 
   return (
     <div css={css`
+    position: static;
     margin: 10px 5px 5px;
     
     `}>
@@ -47,17 +49,56 @@ export function SelectLang () {
   );
 }
 
+export function TlansText () {
+  const classes = useStyles(); 
+  let Test = 'test';
+  // value = {Test};
+  return (
+    <div>
+      <div>
+        <div css={css`
+        width: 100%;
+        margin: 0 8px;
+        `}>
+          <TextField fullWidth variant="outlined" label="textform1"   />
+        </div>
+        <div css={css`
+        width: 100%;
+        margin: 0 8px;
+        `}>
+          <TextField fullWidth variant="outlined" label="textform2"   />
+        </div>
+        <Button variant="contained" color="secondary">送信</Button>
+      </div>
+    </div>
+  );
+
+// constructor(props) {
+//     super(props);
+//     this.state = {
+//       textform1: "textform1",
+//       textform2: "textform2",
+//     }
+// }
+}
+  
+
+
 export default class Try3 extends React.Component {
   render (){
     return (
       <div css={css`
       background-color: #eee;
       padding: 5px;
-      position: relative;
+      position: static;
 
       `}>
-        <SelectLang />
-        <SelectLang />
+        <div>
+          <SelectLang /><SelectLang />  
+        </div>
+        <div>
+          <TlansText />
+        </div>
       </div>
     );  
   }
